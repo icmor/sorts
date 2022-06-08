d8704b94189e2dd14fe132a3959e80597b4cb092
icmor <cornejodlm@ciencias.unam.mx>
2022-06-08 17:33:46 -0500

    Benchmark

---
+++ benchmark.py
@@ -0,0 +1,16 @@
+from sorts import *
+from random import random
+from time import time
+
+def benchmark_sorts(funcs):
+    l = [int(1_000_000 * random()) for i in range(max(funcs.values()))]
+    for func, length in funcs.items():
+        s = l[:length] if length < len(l) else l.copy()
+        start = time()
+        func(s)
+        total = time() - start
+        print(f"{func.__name__}: ran on {length:,} elements "
+              f"in {round(total, 2)} seconds")
+
+if __name__ == "__main__":
+    benchmark_sorts({bubble_sort: 10_000})
